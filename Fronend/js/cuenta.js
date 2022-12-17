7214dbc7e654e4f6c39c44dd21eba286e3aa7611
Angelkhx <48416562+Angelkhx@users.noreply.github.com>
2022-12-17 18:30:05 -0500

    Frontend

---
+++ Fronend/js/cuenta.js
@@ -0,0 +1,2 @@
+const url = "http://localhost:8080/Cuenta/"
+const url1 = "http://localhost:8080/Cuenta/list"
---
+++ Fronend/js/transaccion.js
@@ -0,0 +1,2 @@
+const url = "http://localhost:8080/Transaccion/"
+const url1 = "http://localhost:8080/Transaccion/list"
